bc758d02282fbd592507e2aeb42862ff41dd3db4
gbassisp <guilherme.bassi.sp@gmail.com>
2019-09-06 22:53:46 +0930

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='sqly-me',
+    version='0.0.0.1',
+    packages=['sqly',],
+    authors=['Guilherme Bassi da Silva Pontes', 'Gildeci dos Santos Silva'],
+    email='guilherme.bassi.sp@gmail.com',
+    license=open('LICENSE').read(),
+    long_description=open('README.md').read(),
+)
