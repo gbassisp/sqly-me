ca99a152522bb6f08f3004d3152db19fb9483bf3
gbassisp <guilherme.bassi.sp@gmail.com>
2019-09-06 22:52:13 +0930

    Create __init__.py

---
+++ sqly/__init__.py
@@ -0,0 +1,32 @@
+'''
+The sqly module does not initialize by itself
+It only lists the available input CSV files
+
+It is the user duty to tell the module to convert the files
+'''
+
+greeting_message = '''
+
+Welcome to the SqlyMe project
+
+This project is an open source (under the Apache License, see the docs) solution to the GovHack event, 
+held in South Australia 2019, but it should be useful to use in other cases
+
+I hope it is good enough...
+
+Cheers,
+Guilherme Bassi Pontes
+
+'''.lstrip()
+print(greeting_message)
+
+
+#Start here
+
+from . import readcsv, inputfiles, combinedataframe
+in_path = inputfiles.in_path
+out_path = inputfiles.out_path
+
+available_csv_files = inputfiles.get_in_files(in_path, csv_only=True)
+
+
